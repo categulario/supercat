c91054f6dda11c88a29e8769eb2f61b4b596099f
Abraham Toriz Cruz <a.wonderful.code@gmail.com>
2015-05-08 23:48:09 -0500

    take advantage on boxes function

--- players/lucky.py
+++ players/lucky.py
@@ -10,23 +10,26 @@ def shuffle(iterable):
     random.shuffle(items)
     return items
 
+def boxes():
+    return ((row, col) for row in range(3) for col in range(3))
+
+def random_boxes():
+    return shuffle((row, col) for row in range(3) for col in range(3))
+
 def play(world, game, id, move_num):
     # lazy game
     if game is not None:
         # should play this game
-        for row in shuffle(range(0, 3)):
-            for col in shuffle(range(0, 3)):
-                if world[game][row, col] == None:
-                    return game, (row, col)
+        for row, col in random_boxes():
+            if world[game][row, col] == None:
+                return game, (row, col)
     else:
         # free play!
-        for grand_row in shuffle(range(0, 3)):
-            for grand_col in shuffle(range(0, 3)):
-                if world[grand_row, grand_col]['owner'] is not None:
-                    continue
+        for grand_row, grand_col in random_boxes():
+            if world[grand_row, grand_col]['owner'] is not None:
+                continue
 
-                for row in shuffle(range(0, 3)):
-                    for col in shuffle(range(0, 3)):
-                        if world[grand_row, grand_col][row, col] == None:
-                            return (grand_row, grand_col), (row, col)
+            for row, col in random_boxes():
+                if world[grand_row, grand_col][row, col] == None:
+                    return (grand_row, grand_col), (row, col)
     return None, None
--- players/ordered.py
+++ players/ordered.py
@@ -5,23 +5,23 @@ The ordered player, plays the first available move from top to bottom, from left
 to right
 """
 
+def boxes():
+    return ((row, col) for row in range(3) for col in range(3))
+
 def play(world, game, id, move_num):
     # lazy game
     if game is not None:
         # should play this game
-        for row in range(0, 3):
-            for col in range(0, 3):
-                if world[game][row, col] == None:
-                    return game, (row, col)
+        for row, col in boxes():
+            if world[game][row, col] == None:
+                return game, (row, col)
     else:
         # free play!
-        for grand_row in range(0, 3):
-            for grand_col in range(0, 3):
-                if world[grand_row, grand_col]['owner'] in not None:
-                    continue
+        for grand_row, grand_col in boxes():
+            if world[grand_row, grand_col]['owner'] in not None:
+                continue
 
-                for row in range(0, 3):
-                    for col in range(0, 3):
-                        if world[grand_row, grand_col][row, col] == None:
-                            return (grand_row, grand_col), (row, col)
+            for row, col in boxes():
+                if world[grand_row, grand_col][row, col] == None:
+                    return (grand_row, grand_col), (row, col)
     return None, None
