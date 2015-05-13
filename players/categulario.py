4b6a9a74c163a5b0a4696d5bf070641af54f6067
Abraham Toriz Cruz <a.wonderful.code@gmail.com>
2015-05-12 22:56:34 -0500

    adapt categulario to objects

--- players/categulario.py
+++ players/categulario.py
@@ -1,19 +1,24 @@
 from supercat.utils import random_boxes
+from supercat.classes import BasePlayer
 import random
 
-def play(world, game, identity, move_num):
-    if move_num == 1:
-        return (1, 1), (1, 1)
-    elif move_num < 15:
-        # basic logic, close games if posible, play the best
-        if move_num == 1:
-            return (1, 1), (1, 1)
+class Player(BasePlayer):
+    name = 'categulario'
 
-        return game,
-    elif move_num < 35:
-        # prevent the enemy for closing games, try to make an strategy
-        pass
-    else:
-        # Keep an eye on a winner game
-        pass
-    return None, None
+    def play(world, game, identity, move_num):
+        if move_num == 1:
+            return (random.choice(random_boxes()),)*2
+        elif move_num < 15:
+            # basic logic, close games if posible, play the best
+            if move_num == 1:
+                return (1, 1), (1, 1)
+
+            return None, None
+        elif move_num < 35:
+            # prevent the enemy for closing games, try to make an strategy
+            pass
+        else:
+            # Keep an eye on a winner game
+            pass
+
+        return None, None
