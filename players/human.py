37e5c94efd3d1543503fc6ac60f30252dbf1e808
Abraham Toriz Cruz <a.wonderful.code@gmail.com>
2015-05-14 22:40:27 -0500

    validate input

--- players/human.py
+++ players/human.py
@@ -7,14 +7,28 @@ from supercat.classes import BasePlayer
 
 class Player(BasePlayer):
 
-    name = 'bourne'
+    name = 'human'
+
+    def get_valid_coords(self, thing):
+        while True:
+            try:
+                game = tuple(map(
+                    lambda x:int(x),
+                    input('%s: '%thing).split(' ')
+                ))
+                for i in game:
+                    if i not in (0, 1, 2):
+                        raise ValueError()
+                return game
+            except ValueError:
+                print ('%s inválido'%thing)
 
     def play(self, world, game, move_num, last_move):
         try:
             if game is None:
-                game = tuple(map(lambda x:int(x), input('Juego: ').split(' ')))
+                game = self.get_valid_coords('Juego')
 
-            box  = tuple(map(lambda x:int(x), input('Casilla: ').split(' ')))
+            box  = self.get_valid_coords('Movimiento')
 
             return game, box
         except KeyboardInterrupt:
