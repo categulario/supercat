ba3258f74ebcfec98b22920e50305dd960be73dd
KYSXD <dracojan_12@hotmail.com>
2018-08-10 22:15:28 -0500

    Minor edit to ordered.py for lint

--- players/ordered.py
+++ players/ordered.py
@@ -1,11 +1,13 @@
 """
 # Ordered Player
 
-The ordered player, plays the first available move from top to bottom, from left
-to right
+The ordered player,
+plays the first available move from top to bottom,
+from left to right
 """
 from supercat.classes import BasePlayer
 from supercat.utils import boxes
+
 
 class Player(BasePlayer):
 
@@ -16,7 +18,7 @@ class Player(BasePlayer):
         if game is not None:
             # should play this game
             for row, col in boxes():
-                if world[game][row, col] == None:
+                if world[game][row, col] is None:
                     return game, (row, col)
         else:
             # free play!
@@ -25,6 +27,6 @@ class Player(BasePlayer):
                     continue
 
                 for row, col in boxes():
-                    if world[grand_row, grand_col][row, col] == None:
+                    if world[grand_row, grand_col][row, col] is None:
                         return (grand_row, grand_col), (row, col)
         return None, None
